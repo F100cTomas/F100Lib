6361bc9b0839968fd22a93aa1472b48bb7f9648b
F100cTomas <145491617+F100cTomas@users.noreply.github.com>
2024-10-03 16:32:37 +0200

    First test

---
+++ include/F100Lib.hpp
@@ -0,0 +1,6 @@
+#ifndef F100LIB_HPP
+#define F100LIB_HPP
+namespace F100Lib {
+void hello();
+}
+#endif
---
+++ src/string.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "F100Lib.hpp"
+namespace F100Lib {
+void hello() {
+  std::cout << "Hello World!\n";
+}
+}
